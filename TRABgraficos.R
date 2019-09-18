571785015ba07ac22e1c5cf13766a0c2c9398a96
P1NTSU <lucasmigliorin@hotmail.com>
2019-09-18 10:49:05 -0400

    TRAB

--- TRABgraficos.R
+++ TRABgraficos.R
@@ -30,8 +30,9 @@ quant <- c(lenEng,lenLic,lenMete)
 nome <- c("Engenharia","Lic. em Computação","Meteorologia","Total")
 frequencias <- c(lenEng,lenLic,lenMete,(80))
 porcentagem <- c(((lenEng*100)/80),((lenLic*100)/80),((lenMete*100)/80),(100))
+tabelaCurso <- data.frame("Curso"=nome,"Frequências"=frequencias,"Percentagem"=porcentagem )
 barplot(quant,names.arg = c("Engenharia","Lic. em Computação","Meteorologia"),main = "Quantidade de alunos por curso 2017",xlab = "Curso")
-tabelaCurso <- data.frame("Nome"=nome,"Frequências"=frequencias,"Percentagem"=porcentagem )
+
 
 
 #grafico ensinoMedio e tabela
@@ -42,9 +43,9 @@ tecnica <- length(vet[vet$Cursou_EnsinoMedio == "Escola Técnica",]$Registros)
 nomeM <- c("Militar","Particular","Pública","Técnica","Total")
 frequenciasM <- c(militar,particular,publica,tecnica,(80))
 porcentagemM <- c(((militar*100)/80),((particular*100)/80),((publica*100)/80),((tecnica*100)/80),(100))
+tabelaMedio <- data.frame("Escola"=nomeM,"Frequências"=frequenciasM,"Percentagem"=porcentagemM )
 barplot(c(militar,particular,publica,tecnica),names.arg = c("Militar","Particular","Pública Normal","Técnica"),main = "Local onde foi feito o Ensino Médio 2017",xlab = "Escola")
-tabelaMedio <- data.frame("Nome"=nomeM,"Frequências"=frequenciasM,"Percentagem"=porcentagemM )
-view(tabelaMedio)
+
 
 #grafico genero e tabela
 homem <-length(vet[vet$Genero == "Masculino",]$Registros)
@@ -52,14 +53,19 @@ mulher <-length(vet[vet$Genero == "Feminino",]$Registros)
 nomeG <- c("Homem","Mulher","Total")
 frequenciasG <- c(homem,mulher,(80))
 porcentagemG <- c(((homem*100)/80),((mulher*100)/80),(100))
-tabelaGenero <- data.frame("Nome"=nomeG,"Frequências"=frequenciasG,"Percentagem"=porcentagemG )
+tabelaGenero <- data.frame("Gênero"=nomeG,"Frequências"=frequenciasG,"Percentagem"=porcentagemG )
 barplot(c(homem,mulher),names.arg = c("Masculino","Feminino"),main = "Gênero dos estudantes 2017",xlab = "Gênero")
 #----boxplot(vet$Idade~vet$Genero)-----Opção 2 de gráfico
 #---->pie(table(vet$Genero),main = "Gênero dos estudantes 2017")<----Opção 3 de grafico
 
-#grafico trabalhaSimOuNao
+
+#grafico trabalhaSimOuNao e tabela
 sim <-length(vet[vet$Trabalha == "Sim",]$Registros)
 nao <-length(vet[vet$Trabalha == "Não",]$Registros)
+nomeSN <- c("Homem","Mulher","Total")
+frequenciasSN <- c(sim,nao,(80))
+porcentagemSN <- c(((sim*100)/80),((nao*100)/80),(100))
+tabelaSimNao <- data.frame("Resposta"=nomeSN,"Frequências"=frequenciasSN,"Percentagem"=porcentagemSN )
 barplot(c(sim,nao),names.arg = c("Trabalham","Não Trabalham"),main = "Estudantes que trabalham 2017",xlab = "Respostas")
 
 #grafico HoraDeEstudo
